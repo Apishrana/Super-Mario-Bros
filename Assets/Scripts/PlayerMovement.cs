ce13f3de9866916e6ffffddd09371cd13a7f44da
Apishrana <apishrana682@gmail.com>
2026-05-02 18:44:37 +0530

    init

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float Speed;
+
+    [SerializeField]
+    private float JumpForce;
+
+    private 
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
