eb440f5c2a735c41f86db5cdf898d2eb127b1103
Apishrana <apishrana682@gmail.com>
2026-05-03 15:04:13 +0530

    added basic player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,16 +8,23 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float JumpForce;
 
-     
+    private Rigidbody2D rb;
 
     void Start()
     {
-
+        rb = transform.GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+    }
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(Speed * Input.GetAxis("Horizontal"), rb.linearVelocityY);
+    }
 
+    void OnJump()
+    {
+        rb.AddForceY(JumpForce, ForceMode2D.Impulse);
     }
 }
