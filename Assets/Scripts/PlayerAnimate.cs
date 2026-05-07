b62fa5f62528f250ba2ab128422a170571b1b1dd
Apishrana <apishrana682@gmail.com>
2026-05-07 14:51:07 +0530

    Added animations script to player

--- Assets/Scripts/PlayerAnimate.cs
+++ Assets/Scripts/PlayerAnimate.cs
@@ -1,5 +1,58 @@
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class PlayerAnimate : MonoBehaviour
 {
+    public Animator animator;
+
+
+    void Start()
+    {
+        animator = transform.GetComponent<Animator>();
+    }
+
+    public void Walk()
+    {
+        animator.SetBool("Walking", true);
+    }
+    public void StopWalk()
+    {
+        animator.SetBool("Walking", false);
+    }
+    public void Jump()
+    {
+        animator.SetBool("Jumping", true);
+        animator.SetBool("Walking", false);
+    }
+    public void StopJump()
+    {
+        animator.SetBool("Jumping", false);
+    }
+    public void Sprint()
+    {
+        animator.SetFloat("WalkSpeed", 1.25f);
+    }
+    public void StopSprint()
+    {
+        animator.SetFloat("WalkSpeed", 1f);
+    }
+
+    public async Task grow()
+    {
+        animator.SetBool("Unkillable", true);
+        animator.SetTrigger("Grow");
+        animator.ResetTrigger("Grow");
+        await Awaitable.WaitForSecondsAsync(0.7f, destroyCancellationToken);
+        animator.SetBool("Unkillable", false);
+    }
+
+    public async Task shrink()
+    {
+        animator.SetBool("Unkillable", true);
+        animator.SetTrigger("Shrink");
+        animator.ResetTrigger("Shrink");
+        await Awaitable.WaitForSecondsAsync(0.7f, destroyCancellationToken);
+        animator.SetBool("Unkillable", false);
+    }
+
 }
